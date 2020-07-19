1469b676ff3c124e5a95eae8abd167a810d34a9f
Geoff Hamilton <hamilton@computing.dcu.ie>
2020-07-19 15:01:37 +0100

    Update Main.hs
    Updated to latest version.

--- src/Main.hs
+++ src/Main.hs
@@ -4,7 +4,6 @@ module Main (
 
 import Term
 import Tree
-import Debug.Trace
 import System.Directory
 import System.IO
 import System.Timeout
@@ -54,56 +53,52 @@ toplevel p = do putStr "LOG> "
                                            case parseProg c of
                                               Left s -> do putStrLn $ "Could not parse term in file "++f++".pl: "++ show s
                                                            toplevel p
-                                              Right t -> do putStrLn $ "Loading file: "++f++".pl"
-                                                            toplevel $ Just t
+                                              Right (t,cs) -> toplevel $ Just (t,cs)
                                    else do putStrLn $ "No such file: "++f++".pl"
                                            toplevel Nothing
                    Prog -> case p of
                               Nothing -> do putStrLn "No program loaded"
                                             toplevel p
-                              Just prog  -> do putStrLn $ showprog prog
-                                               toplevel p
+                              Just (t,d)  -> do putStrLn $ showprog (t,d)
+                                                toplevel p
                    Eval -> case p of
                               Nothing -> do putStrLn "No program loaded"
                                             toplevel p
-                              Just (ts,cs)  -> f (foldr varsTerm' [] ts) ts cs []
-                                               where
-                                               f [] ts cs env = let ts' = map (instantiateTerm env) ts
-                                                                    xs = foldr varsTerm' [] ts'
-                                                                    tss = eval (map Var xs) ts' cs
-                                                                in  case tss of
-                                                                       [] -> do putStrLn "False"
-                                                                                toplevel p
-                                                                       _ -> do putStrLn "True"
-                                                                               g xs tss
-                                                                               where
-                                                                               g xs [] = toplevel p
-                                                                               g xs (ts:tss) = h xs ts
-                                                                                               where
-                                                                                               h [] [] = g xs tss
-                                                                                               h (x:xs) (t:ts) = do putStrLn $ x++" = "++show t
-                                                                                                                    h xs ts
-                                               f (x:xs) ts cs env = do putStr $ x++" = "
-                                                                       hFlush stdout
-                                                                       l <-  getLine
-                                                                       case parseTerm l of
-                                                                          Left s -> do putStrLn $ "Could not parse term: "++ show s
-                                                                                       f (x:xs) ts cs env
-                                                                          Right u -> if   u == Var x
-                                                                                     then f xs ts cs env
-                                                                                     else f xs ts cs ((x,u):env)
+                              Just (t,d) -> f (vars t) t [] d
+                                            where
+                                            f [] t e d = let t' = walk e t
+                                                             xs = vars t'
+                                                             rs = eval t' d
+                                                         in  case rs of
+                                                                [] -> do putStrLn "False"
+                                                                         toplevel p
+                                                                _ -> do putStrLn "True"
+                                                                        g xs rs
+                                                                        where
+                                                                        g xs [] = toplevel p
+                                                                        g xs (r:rs) = h xs r
+                                                                                      where
+                                                                                      h [] [] = g xs rs
+                                                                                      h (x:xs) (t:ts) = do putStrLn $ x++" = "++show t
+                                                                                                           h xs ts
+                                            f (x:xs) t e d = do putStr $ x++" = "
+                                                                hFlush stdout
+                                                                l <-  getLine
+                                                                case parseTerm l of
+                                                                   Left s -> do putStrLn $ "Could not parse term: "++ show s
+                                                                                f (x:xs) t e d
+                                                                   Right u -> if   u == Var x
+                                                                              then f xs t e d
+                                                                              else f xs t ((x,u):e) d
 
                    Trans -> case p of
                                Nothing -> do putStrLn "No program loaded"
                                              toplevel p
-                               Just (ts,cs)  -> do let (ts',cs') = residualise $ trans ts cs
-                                                   let cs'' = sortBy (comparing (atomName.fst)) cs'
-                                                   putStrLn $ showprog (ts',cs'')
-                                                   toplevel $ Just (ts',cs'')
+                               Just (t,d) -> do let p' = trans (t,d)
+                                                putStrLn $ showprog p'
+                                                toplevel $ Just p'
                    Quit -> return ()
                    Help -> do putStrLn helpMessage
                               toplevel p
                    Unknown -> do putStrLn "Err: Could not parse command, type ':help' for a list of commands"
                                  toplevel p
-
-
